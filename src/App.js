ebe5ba05037fd12ffd97853391de4d782c47e718
Bair <73276298+zoxione@users.noreply.github.com>
2021-11-25 16:11:33 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
 import React from "react";
 import './App.css';
 
-import Menu from "./components/Menu";
+
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.start = this.start.bind(this);
+    this.state = {page: 0, lang: "ru"};
+  }
 
+  start() {
+    //this.setState({page: this.state.page + 1});
+    this.setState(function(prevState, props) {
+      return {
+        page: prevState.page + 1
+      };
+    });
+    console.log(this.state.page);
+  }
 
   render () {
     return(
       <div>
+        <div className="app">
           <h1>Blockchain Learn</h1> 
+          <button className="button_start" onClick={this.start}>Играть</button>
+        </div>
+        
       </div>
     )
   };
