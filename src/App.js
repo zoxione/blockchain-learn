5db952ddc962f3784df4255d55c8fff31c57bf80
Bair <73276298+zoxione@users.noreply.github.com>
2021-11-25 15:04:31 +0300

    add home page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          test
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Menu from "./components/Menu";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.start = this.start.bind(this);
+    this.state = {page: 0, lang: "ru"};
+  }
+
+  start() {
+    //this.setState({page: this.state.page + 1});
+    this.setState(function(prevState, props) {
+      return {
+        page: prevState.page + 1
+      };
+    });
+    console.log(this.state.page);
+  }
+
+  render () {
+    return(
+      <div>
+        <div className="app">
+          <h1>Blockchain Learn</h1> 
+          <button className="button_start" onClick={this.start}>Играть</button>
+        </div>
+        <Menu />
+      </div>
+    )
+  };
 }
 
 export default App;
