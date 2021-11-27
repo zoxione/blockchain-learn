e9f1363b83197e7acbb4c3b01e41593c28de89dc
Bair <73276298+zoxione@users.noreply.github.com>
2021-11-27 12:50:41 +0300

    tabs fix

--- src/App.js
+++ src/App.js
@@ -10,30 +10,30 @@ import Page3 from "./pages/Page3";
 import Page4 from "./pages/Page4";
 
 function App() {
-  const page = useSelector(state => state.page);
+    const page = useSelector(state => state.page);
 
-  return (
-    <div>
-      <div className="main">
-        {page === 0 &&
-          <Page0 />
-        }
-        {page === 1 &&
-          <Page1 />
-        }
-        {page === 2 &&
-          <Page2 />
-        }
-        {page === 3 &&
-          <Page3 />
-        }
-        {page === 4 &&
-          <Page4 />
-        }
-      </div>
-      <Menu />
+    return (
+    <div className="app">
+        <div className="main">
+            {page === 0 &&
+                <Page0 />
+            }
+            {page === 1 &&
+                <Page1 />
+            }
+            {page === 2 &&
+                <Page2 />
+            }
+            {page === 3 &&
+                <Page3 />
+            }
+            {page === 4 &&
+                <Page4 />
+            }
+        </div>
+        <Menu />
     </div>
-  )
+    )
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -6,29 +6,29 @@ import { Provider } from 'react-redux';
 import App from './App';
 
 const defaultState = {
-  page: 0,
-  language: "rus",
-  sound: true,
+    page: 0,
+    language: "rus",
+    sound: true,
 }
 
 const reducer = (state = defaultState, action) => {
-  switch (action.type) {
-    case "SET_PAGE":
-      return {...state, page: action.payload}
-    case "TOGGLE_LANGUAGE":
-      return {...state, language: state.language === "rus" ? "eng" : "rus"}
-    case "TOGGLE_SOUND":
-      return {...state, sound: state.sound === true ? false : true}
-    default:
-      return state;
-  }
+    switch (action.type) {
+        case "SET_PAGE":
+            return {...state, page: action.payload}
+        case "TOGGLE_LANGUAGE":
+            return {...state, language: state.language === "rus" ? "eng" : "rus"}
+        case "TOGGLE_SOUND":
+            return {...state, sound: state.sound === true ? false : true}
+        default:
+            return state;
+    }
 }
 
 const store = createStore(reducer);
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
 );
